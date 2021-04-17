e57dd86576ce6309d7fc5371021e9d6bf0ed716d
v-randranto <verorand@yahoo.com>
2021-04-17 21:57:05 +0200

    post souvenirs operational

---
+++ src/components/Posts/Posts.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Grid, Typography } from '@material-ui/core';
+import { useSelector } from 'react-redux';
+
+import Post from './Post/Post';
+import useStyles from './styles';
+
+const Posts = ({ setCurrentId }) => {
+  const posts = useSelector((state) => state.posts);
+  const classes = useStyles();
+
+  return (
+    !posts.length ? 
+    <Typography className={classes.heading} variant="h5" >Souvenirs, souvenirs...</Typography> 
+    : (
+      <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+        {posts.map((post) => (
+          <Grid key={post._id} item xs={12} sm={6} md={6}>
+            <Post post={post} setCurrentId={setCurrentId} />
+          </Grid>
+        ))}
+      </Grid>
+    )
+  );
+};
+
+export default Posts;
