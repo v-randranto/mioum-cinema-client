343b6245db09219847c0d46fc72fff3912d95546
v-randranto <verorand@yahoo.com>
2021-04-24 01:20:03 +0200

    add circular progress

--- src/components/Films/Film/Presentation.js
+++ src/components/Films/Film/Presentation.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
--- src/components/Films/Films.js
+++ src/components/Films/Films.js
@@ -2,27 +2,34 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 import Grid from '@material-ui/core/Grid';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 import Film from './film/Film';
-import useSortFilms from './useSortFilms'
+import useSortFilms from './useSortFilms';
 
 const Films = ({ setCurrentId, open, handleOpen, sortCriterion }) => {
   const films = useSelector((state) => state.films);
-  useSortFilms(sortCriterion, films)
+  useSortFilms(sortCriterion, films);
 
   return (
-    <Grid container spacing={8}>
-      {films.map((film) => (
-        <Grid item key={film._id} xs={12} md={2}>
-          <Film
-            film={film}
-            setCurrentId={setCurrentId}
-            open={open}
-            handleOpen={handleOpen}
-          />
+    <>
+      {!films && <CircularProgress />}
+
+      {films && (
+        <Grid container spacing={8}>
+          {films.map((film) => (
+            <Grid item key={film._id} xs={12} md={2}>
+              <Film
+                film={film}
+                setCurrentId={setCurrentId}npm start
+                open={open}
+                handleOpen={handleOpen}
+              />
+            </Grid>
+          ))}
         </Grid>
-      ))}
-    </Grid>
+      )}
+    </>
   );
 };
 
