2d79be73d756f13fd20f13198bae32fc99887056
v-randranto <verorand@yahoo.com>
2021-05-02 08:04:34 +0200

    fix/sortHandler

--- src/utils/sortHandler.js
+++ src/utils/sortHandler.js
@@ -1,4 +1,4 @@
-const pattern = /^(a\s|à\sla\s|aux\s|ce\s|ces\s|cet\s|cette\s|de\s|des\s|du\s|de la\s| la\s|le\s|les\s|the\s|this\s|that\s|these\s|un\s|une\s)/i
+const pattern = /^(a\s|à\sla\s|aux\s|ce\s|ces\s|cet\s|cette\s|de\s|des\s|du\s|de la\s|l'|la\s|le\s|les\s|the\s|this\s|that\s|these\s|un\s|une\s)/i
 const articleExtract = (str) => {
   let newStr = str.toLowerCase()
   const matchArray = newStr.match(pattern)  
